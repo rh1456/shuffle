7ed553132160be39cea7c3d2d49d59cfe5657cef
Reid Hanson <reidhans@buffalo.edu>
2019-11-06 16:50:11 -0500

    shuffle app

--- public/main.js
+++ public/main.js
@@ -1,7 +1,50 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+const suits = ['hearts', 'clubs', 'spades', 'diamonds']
+
+const rank = [
+  'Ace',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'Jack',
+  'Queen',
+  'King'
+]
+const deck = []
+
+//create the deck
+
+const makeDeck = () => {
+  for (let suitsIndex = 0; suitsIndex < suits.length; suitsIndex++) {
+    for (let rankIndex = 0; rankIndex < rank.length; rankIndex++) {
+      deck.push(rank[rankIndex] + 'of' + suits[suitsIndex])
+    }
   }
 }
+//shuffle the deck
+//use algorithm
+const shuffleDeck = () => {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i)
+    const holder = deck[i]
+    deck[i] = deck[j]
+    deck[j] = holder
+  }
+  console.log(shuffleDeck)
+}
 
-document.addEventListener('DOMContentLoaded', main)
+const dealCard = () => {
+  document.querySelector('p').textContent = deck[0]
+}
+const makeAndShuffleDeck = () => {
+  makeDeck()
+  shuffleDeck()
+}
+
+document.addEventListener('click', dealCard)
+document.addEventListener('DOMContentLoaded', makeAndShuffleDeck)
